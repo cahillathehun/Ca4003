7f93e0c01e1411ad331c379e3c7a5d408ef50221
Alexander Cahill <cahila23@winlabs.com>
2019-10-29 13:55:37 +0000

    updated grammar to take in functions but doesnt work yet and 'build.sh'.

--- Assignment1/build_and_run.sh
+++ Assignment1/build_and_run.sh
@@ -4,6 +4,12 @@ javacc ccalparse.jj
 javac ccalparse.java
 
 java ccalparse tests/main1.ccl
+echo MAIN1 FIN
 java ccalparse tests/main2.ccl
+echo MAIN2 FIN
 java ccalparse tests/main3.ccl
+echo MAIN3 FIN
 java ccalparse tests/comments.ccl
+echo COMMENTS FIN
+java ccalparse tests/simple_func.ccl
+echo SIMPLE_FUNC FIN
