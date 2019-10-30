7ae503c3991c980c8431f07d4391116faf69c3d2
cahila23 <alexander.cahill@mail.dcu.ie>
2019-10-30 13:43:42 +0000

    Updated grammar so that it passes all tests defined in the pdf, fixed syntax in the test, updated shell script to build compile then run tests. TODO Conflict choice warning still exists TODO.

--- Assignment1/build_and_run.sh
+++ Assignment1/build_and_run.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 
+javacc ccalparse.jj
+javac ccalparse.java
+
 java ccalparse tests/main1.ccl
 echo MAIN1 FIN
 java ccalparse tests/main2.ccl
@@ -12,3 +15,5 @@ java ccalparse tests/simple_func.ccl
 echo SIMPLE_FUNC FIN
 java ccalparse tests/scopes.ccl
 echo SCOPES FIN
+java ccalparse tests/functions.ccl
+echo FUNCTIONS FIN
