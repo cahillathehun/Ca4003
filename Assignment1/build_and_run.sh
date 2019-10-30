0149bfaafd08c0c546c47bd77f8ba7d53dfcd57a
cahila23 <alexander.cahill@mail.dcu.ie>
2019-10-30 12:32:38 +0000

    updated grammar and added new test.

--- Assignment1/build_and_run.sh
+++ Assignment1/build_and_run.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env sh
 
-javacc ccalparse.jj
-javac ccalparse.java
-
 java ccalparse tests/main1.ccl
 echo MAIN1 FIN
 java ccalparse tests/main2.ccl
@@ -13,3 +10,5 @@ java ccalparse tests/comments.ccl
 echo COMMENTS FIN
 java ccalparse tests/simple_func.ccl
 echo SIMPLE_FUNC FIN
+java ccalparse tests/scopes.ccl
+echo SCOPES FIN
